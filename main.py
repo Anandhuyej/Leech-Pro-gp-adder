544085923b7ba35b7ddd4d1d4b5224744ae52669
Anandhuyej <88794006+Anandhuyej@users.noreply.github.com>
2022-05-12 14:41:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ bot = Dispatcher(K)
 #https://github.com/BotfatherDev/CommonChatModer/blob/work_in_groups/filters/chat_filters.py#L10
 class IsPrivate(BoundFilter):
     async def check(self, message: types.Message):
-        return message.chat.type == types.ChatType.PRIVATE
+        return message.chat.type == types.ChatType.supergroup
 
 
 @bot.message_handler(IsPrivate(), Command("start", prefixes="/"))
